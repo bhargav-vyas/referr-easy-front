90b03c0f7791581566e653655873ecd6976c1d5e
Bhargav <vyasb1503@gmail.com>
2025-04-26 22:56:56 +0530

    this is the home page insert in the  components

---
+++ src/components/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+function Home(){
+    return(
+        <div>
+
+          <h1> Welcome to referrEasy</h1>
+          <Link></Link>
+        </div>
+    )
+
+}
