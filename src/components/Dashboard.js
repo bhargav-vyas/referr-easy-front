dd594057dddc8f7ee8da9f4036bab416ba7ca83f
Bhargav <vyasb1503@gmail.com>
2025-04-30 22:53:03 +0530

    this is the code for the  Dashboard .js

---
+++ src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      <p>Welcome to your dashboard!</p>
+      {/* Later: Show referral stats, profile, etc */}
+    </div>
+  );
+}
+
+export default Dashboard;
