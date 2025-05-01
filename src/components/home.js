90fc16b63c599138633d6e678a8b0eb7826337e7
Bhargav <vyasb1503@gmail.com>
2025-05-01 23:02:52 +0530

    this is the  code of the  css for the home and also change in the  home js

--- src/components/home.js
+++ src/components/home.js
@@ -1,14 +1,31 @@
-import React from "react";
-import { Link } from "react-router-dom";
-function Home(){
-    return(
-        <div>
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './Home.css';
 
-          <h1> Welcome to referrEasy</h1>
-          <Link to="./Register.js"></Link> 
-          <Link to ="./Login.js"></Link>
-        </div>
-    );
+function Home() {
+  const [message, setMessage] = useState('');
 
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/home/message')
+      .then(response => {
+        setMessage(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching home message:', error);
+        setMessage('Failed to load welcome message.');
+      });
+  }, []);
+
+  return (
+    <div className="home-container">
+      <h1 className="home-title">ReferrEasy</h1>
+      <p className="home-message">{message}</p>
+      <div className="home-buttons">
+        <a href="/register" className="home-btn">Register</a>
+        <a href="/login" className="home-btn">Login</a>
+      </div>
+    </div>
+  );
 }
+
 export default Home;
