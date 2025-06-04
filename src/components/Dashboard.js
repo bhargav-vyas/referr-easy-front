59e2a564df4a5004c24a674f856d22d8ab10316b
Bhargav <vyasb1503@gmail.com>
2025-06-04 23:42:35 +0530

    this  is the changes in  dashbord

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,14 @@
-// import React from 'react';
-// import './dashboard.css'; // Import the CSS file
+import React from 'react';
+import './dashboard.css'; // Import the CSS file
 
-// function Dashboard() {
-//   return (
-//     <div className="dashboard-container">
-//       <h2>Dashboard</h2>
-//       <p>Welcome to your dashboard!</p>
-//       {/* Later: Show referral stats, profile, etc */}
-//     </div>
-//   );
-// }
+function Dashboard() {
+  return (
+    <div className="dashboard-container">
+      <h2>Dashboard</h2>
+      <p>Welcome to your dashboard!</p>
+      {/* Later: Show referral stats, profile, etc */}
+    </div>
+  );
+}
 
-// export default Dashboard;
+export default Dashboard;
